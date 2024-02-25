8b328d33a5166d38849a06647a94226142d57121
Reid <reid1@me.com>
2024-02-24 19:49:15 -0500

    adding some ducky scripts

--- duckietest.py
+++ duckietest.py
@@ -10,8 +10,9 @@ robot: DB21J = DB21J(SIMULATED_ROBOT_NAME, simulated=True)  # change accordingly
 
 
 # move wheels
-speeds = (0.5, 0.5)
+speeds = (-0.5, -0.5)
 robot.motors.start()
+# robot.motors.stop()
 stime: float = time.time()
 while time.time() - stime < 4:
     robot.motors.publish(speeds)
@@ -21,20 +22,20 @@ robot.motors.stop()
 print("Stopped.")
 
 
-# LED lights show
-frequency: float = 1.4
-off: RGBAColor = (0, 0, 0, 0.0)
-amber: RGBAColor = (1, 0.7, 0, 1.0)
-lights_on: LEDsPattern = LEDsPattern(front_left=amber, front_right=amber, rear_right=amber, rear_left=amber)
-lights_off: LEDsPattern = LEDsPattern(front_left=off, front_right=off, rear_right=off, rear_left=off)
-pattern: List[LEDsPattern] = [lights_on, lights_off]
-robot.lights.start()
-stime: float = time.time()
-i: int = 0
-while time.time() - stime < 8:
-    lights: LEDsPattern = pattern[i % 2]
-    robot.lights.publish(lights)
-    time.sleep(1. / frequency)
-    i += 1
-robot.lights.stop()
-print("Stopped.")      
+# # LED lights show
+# frequency: float = 1.4
+# off: RGBAColor = (0, 0, 0, 0.0)
+# amber: RGBAColor = (1, 0.7, 0, 1.0)
+# lights_on: LEDsPattern = LEDsPattern(front_left=amber, front_right=amber, rear_right=amber, rear_left=amber)
+# lights_off: LEDsPattern = LEDsPattern(front_left=off, front_right=off, rear_right=off, rear_left=off)
+# pattern: List[LEDsPattern] = [lights_on, lights_off]
+# robot.lights.start()
+# stime: float = time.time()
+# i: int = 0
+# while time.time() - stime < 8:
+#     lights: LEDsPattern = pattern[i % 2]
+#     robot.lights.publish(lights)
+#     time.sleep(1. / frequency)
+#     i += 1
+# robot.lights.stop()
+# print("Stopped.")      
---
+++ scripts/duckyalwaysdrive.py
@@ -0,0 +1,82 @@
+import time
+from typing import Tuple, List
+from duckietown.sdk.robots.duckiebot import DB21J
+from duckietown.sdk.types import LEDsPattern, RGBAColor
+from pynput import keyboard
+
+SIMULATED_ROBOT_NAME: str = "map_0/vehicle_0"
+REAL_ROBOT_NAME: str = "rover"
+
+robot: DB21J = DB21J(SIMULATED_ROBOT_NAME, simulated=True)  # change accordingly
+
+# Define global variables for speeds and movement flags
+speeds = [0,0];
+straight = [0.5,0.5]
+pressed = False;
+
+# def update_speeds(spd):
+#     global speeds
+#     speeds = spd;
+#     return
+
+def accelerate(max=1):
+    global speeds
+    for i in range(10):
+        speeds[0] = ((i+1)/10)*max;
+        speeds[1] = ((i+1)/10)*max;
+        robot.motors.publish(tuple(speeds))
+        time.sleep(0.25)
+    print('done')
+    return
+
+
+def on_press(key):
+    #global speeds, moving_forward, moving_backward
+    global speeds, pressed
+    if pressed:
+        print('held')
+        return
+    pressed = True;
+    if key == keyboard.Key.left:
+        speeds[0] -= 0.2;
+    elif key == keyboard.Key.right:
+        speeds[1] -= 0.2;
+    elif key == keyboard.Key.down:
+        speeds[0] = -straight[0];
+        speeds[1] = -straight[1];
+    return
+
+
+def on_release(key):
+    #global speeds, moving_forward, moving_backward
+    global speeds, pressed
+    print('release')
+    pressed = False;
+    if key == keyboard.Key.left:
+        speeds[0] = straight[0];
+        speeds[1] = straight[1];
+    elif key == keyboard.Key.right:
+        speeds[0] = straight[0];
+        speeds[1] = straight[1];
+    elif key == keyboard.Key.down:
+        speeds[0] = straight[0];
+        speeds[1] = straight[1];
+    return
+
+
+listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+listener.start()
+
+robot.motors.start()
+speeds = straight.copy()
+robot.motors.publish(tuple(speeds))
+time.sleep(0.25)
+
+stime: float = time.time()
+while time.time() - stime < 10:
+    robot.motors.publish(tuple(speeds))
+    print(speeds)
+    time.sleep(0.25)
+
+listener.stop()
+
